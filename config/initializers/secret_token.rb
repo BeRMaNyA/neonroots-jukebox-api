d8c05ec4bf69f1344953c9eb9fc121c6377320c1
Berna Castro <bernacas@gmail.com>
2013-10-31 02:38:33 -0200

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeonrootsJukeboxApi::Application.config.secret_token = '91212a42a106a330dfba50778e63e4582bc190f226644cb1d0fd7e561c575c034526017c42fbb0de1f4df693f5ee896f1c0e78ff66ff1dd299937b35a70abe21'
