004979513ce1f03f40e1d20d9a3bbac8901b4a6f
Berna Castro <bernacas@gmail.com>
2013-10-31 03:25:02 -0200

    Basic rspec implementation

---
+++ spec/basic_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Array do
+  it "should be empty when first created" do
+    subject.should be_empty
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.tty = true
+  config.formatter = :documentation
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before do
+    ActionMailer::Base.deliveries.clear
+  end
+end
